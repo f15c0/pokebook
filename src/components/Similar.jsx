e700ad72ef78eec025ce0fba7b2bd351969029d6
Dennis K <dennisagbokpe@gmail.com>
2023-04-21 13:51:28 -0500

    Similar Pokemons

--- src/components/Similar.jsx
+++ src/components/Similar.jsx
@@ -19,23 +19,26 @@ const SimilarPokemon = ({ targetPokemon, allPokemon }) => {
       <h3 className="text-center pt-0 mt-2 pb-2 sm:text-lg text-md">
         Similar Pokémon
       </h3>
-      <div className="sm:grid grid  sm:grid-cols-2 pb-6 mb-3 gap-6 max-w-[80%] mx-auto">
+      <div className="sm:grid grid  grid-cols-2 pb-6 mb-3 sm:gap-6 sm:max-w-[80%] mx-auto">
         {similarPokemon.map((pokemon) => (
           <Card
             key={pokemon.id}
             shadow="sm"
             padding="xs"
             radius={16}
-            className="relative overflow-visible w-32  sm:w-full mx-auto sm:h-32"
+            className="relative overflow-visible w-24   sm:w-full mx-auto sm:h-32"
           >
-            <div className="h-16 bg-pokemonBg rounded-xl text-center relative">
+            <div className="sm:h-16 h-12 bg-pokemonBg rounded-xl text-center relative">
               <img
                 src={pokemon.sprites.other.dream_world.front_default}
                 alt={pokemon.name}
-                className="absolute -top-2 left-1/2 transform -translate-x-1/2 h-20 -translate-y-4"
+                className="absolute -top-2 left-1/2 transform -translate-x-1/2 h-16 sm:h-20 -translate-y-4"
               />
             </div>
-            <Text weight={500} className="text-center sm:text-md text-sm pt-2">
+            <Text
+              weight={500}
+              className="text-center sm:text-base text-xs pt-2"
+            >
               {pokemon.name}
             </Text>
           </Card>
